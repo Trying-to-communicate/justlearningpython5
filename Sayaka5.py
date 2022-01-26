4091a50e4d70008e20603c82e7214267e283ec6b
Blue <purgekingsubaru@gmail.com]>
2022-01-26 10:08:04 +0530

    Initial commit

---
+++ Sayaka5.py
@@ -0,0 +1,31 @@
+"""
+waifus = ["Isla" , "Sayaka" , "Kuroha Neko" , "Akari" , "002" , "Natsuki"]
+print(waifus[2])
+numbers = [2 ,4 , 9 , 7 ,8]
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+print(numbers[0:5])
+print(numbers[1:2:5])
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+numbers.append(88)
+print(numbers)
+numbers.insert(2 , 44)
+print(numbers)
+numbers.remove(88)
+print(numbers)
+numbers.pop()
+print(numbers)
+numbers[1]=66
+print(numbers)
+tp = (1 , 2 ,3)
+print(tp)
+"""
+a = 3
+b = 4
+a,b = b,a
+print(a ,b )
